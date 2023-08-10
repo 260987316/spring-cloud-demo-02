e843e4ad0d42ef3481f5c36f91bb7db24249b54e
TheChangCHN <TheChangCHN@outlook.com>
2023-08-11 01:09:32 +0800

    内网穿透代码提交

--- common/src/main/java/com/liuchang/common/CommonApplication.java
+++ common/src/main/java/com/liuchang/common/CommonApplication.java
@@ -3,10 +3,20 @@ package com.liuchang.common;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@RestController
 public class CommonApplication {
+
+    @GetMapping
+    public String test() {
+        return "狗子报时:" + new Date();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CommonApplication.class, args);
--- common/src/test/java/com/liuchang/common/CommonApplicationTests.java
+++ common/src/test/java/com/liuchang/common/CommonApplicationTests.java
@@ -3,11 +3,18 @@ package com.liuchang.common;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootTest
 class CommonApplicationTests {
 
     @Test
     void contextLoads() {
+        Map<String,String> map = new HashMap<>();
+        map.put("k","v");
     }
+
+
 
 }
